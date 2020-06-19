7441e60726d69ebfe81b862dcc5ad5558642abc5
Diego Stiehl <seccomiro@gmail.com>
2020-06-19 17:51:32 -0300

    Make controllers reflect the new "cookies" list
    There is no "cookie" anymore at Oven.
    So we need to update controllers and views to reflect the new attribute:
      - Model: Oven
     - Attribute: cookies
     - Possible value: a list of cookies
     P.s. In some locations the code uses to oven.cookis.first:
      - This is OK, because we will always have a sheet full of cookies of the same filling at the oven.
       - Another strategy should be create a new model named Sheet to manage it.

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -2,23 +2,38 @@
 
 class CookiesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_oven, only: %i[new create]
 
   def new
-    @oven = current_user.ovens.find_by!(id: params[:oven_id])
-    if @oven.cookie
-      redirect_to @oven, alert: 'A cookie is already in the oven!'
+    if @oven.cookies.any?
+      redirect_to @oven, alert: 'One or more cookies are already in the oven!'
     else
-      @cookie = @oven.build_cookie
+      @cookie = @oven.cookies.build
     end
   end
 
   def create
-    @oven = current_user.ovens.find_by!(id: params[:oven_id])
-    @cookie = @oven.create_cookie!(cookie_params)
-    redirect_to oven_path(@oven)
+    begin
+      quantity = params[:quantity].to_i
+    rescue StandardError
+      quantity = 0
+    end
+
+    if quantity.positive?
+      quantity.times do
+        @cookie = @oven.cookies.create!(cookie_params)
+      end
+      redirect_to oven_path(@oven)
+    else
+      redirect_to new_oven_cookies_path, alert: 'You must enter a valid quantity!'
+    end
   end
 
   private
+
+  def set_oven
+    @oven = current_user.ovens.find_by!(id: params[:oven_id])
+  end
 
   def cookie_params
     result_params = params.require(:cookie).permit(:fillings)
