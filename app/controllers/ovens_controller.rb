d90d4938c3991b8790e0d18ec2ad3275e65a43c1
Diego Stiehl <seccomiro@gmail.com>
2020-06-19 21:46:59 -0300

    Add an action to return the oven status

--- app/controllers/ovens_controller.rb
+++ app/controllers/ovens_controller.rb
@@ -1,6 +1,6 @@
 class OvensController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_oven, only: %i[show empty]
+  before_action :set_oven, only: %i[show empty status]
 
   def index
     @ovens = current_user.ovens
@@ -17,6 +17,20 @@ class OvensController < ApplicationController
     end
     redirect_to @oven, alert: 'Oven emptied!'
   end
+
+  def status
+    if @oven.cookies.any?
+      render json: {
+        count: @oven.cookies.count,
+        ready: @oven.cookies.first.ready?,
+        fillings: @oven.cookies.first.fillings
+      }
+    else
+      render json: { count: 0 }
+    end
+  end
+
+  private
 
   def set_oven
     @oven = current_user.ovens.find_by!(id: params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resource :cookies
     member do
       post :empty
+      get :status
     end
   end
 end
